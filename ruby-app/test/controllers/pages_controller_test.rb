d0e29e8cfa9624e77056fee558bdb90dea167c43
Trent Walraven <trwbox@gmail.com>
2025-02-13 13:08:14 -0600

    Updating newly pulled code

--- ruby-app/test/controllers/pages_controller_test.rb
+++ ruby-app/test/controllers/pages_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get thankyou" do
-    get pages_thankyou_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
